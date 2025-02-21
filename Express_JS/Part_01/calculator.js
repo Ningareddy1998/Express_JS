5870baed99155d423285e16a53226e975a262e23
Ningareddy Bheemannagari <ningareddy987@gmail.com>
2025-02-21 16:06:06 +0530

    Create calculator.js

---
+++ Express_JS/Part_01/calculator.js
@@ -0,0 +1,43 @@
+/*export const add = (a, b) => a + b
+export const sub = (a, b) => a - b
+//export default add
+//exports.sub = sub*/
+
+//export default 5*3;
+/*
+export default function (num1, num2) {
+  return num1 + num2
+}
+*/
+
+/*export default class StudentDetails {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+}*/
+/*
+let a = 5
+let b = 'Ningeredy'
+export {a, b}*/
+
+/*export function sum(a, b) {
+  return a + b
+}
+export function sub(a, b) {
+  return a - b
+}*/
+
+class StudentDetails {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+}
+class CarDetails {
+  constructor(name, age) {
+    this.name = name
+    this.spedd = age
+  }
+}
+export {StudentDetails, CarDetails}
