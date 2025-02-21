95b0b583b429c2a8c1751ee29bca3473984f20b3
Ningareddy Bheemannagari <ningareddy987@gmail.com>
2025-02-21 16:10:39 +0530

    Create index.mjs

---
+++ Express_JS/Part_01/index.mjs
@@ -0,0 +1,32 @@
+/*import {add, sub} from './calculator.mjs'
+
+console.log(add(10, 3))
+console.log(sub(10, 3))
+*/
+/*import value from './calculator.mjs'
+console.log(value)*/
+
+/*import sum from './calculator.mjs';
+console.log(sum(50,60));
+*/
+
+/*import StudentDetails from './calculator.mjs'
+
+const a = new StudentDetails("Rahul",15);
+console.log(a)
+*/
+
+/*import {a, b} from './calculator.mjs'
+console.log(a)
+console.log(b)*/
+
+/*import {sum,sub} from './calculator.mjs';
+console.log(sum(10,5));
+console.log(sub(10,5));*/
+
+import {StudentDetails, CarDetails} from './calculator.mjs'
+
+const a = new StudentDetails('Ram', 50)
+console.log(a)
+const b = new CarDetails('Audi', 50)
+console.log(b)
